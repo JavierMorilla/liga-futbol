f1c4c90cda229b13452a8934fddd4d55a8993e2c
Morigoll <JavichuMB@gmail.com>
2025-02-27 21:49:14 +0100

    First Dashboard version

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ClasificacionesComponent } from './pages/clasificaciones/clasificaciones.component';
+import { EstadisticasComponent } from './pages/estadisticas/estadisticas.component';
+import { EquiposComponent } from './pages/equipos/equipos.component';
 
-export const routes: Routes = [];
+export const appRoutes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'clasificaciones', component: ClasificacionesComponent },
+  { path: 'estadisticas', component: EstadisticasComponent },
+  { path: 'equipos', component: EquiposComponent }
+];
---
+++ src/app/pages/clasificaciones/clasificaciones.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-clasificaciones',
+  imports: [],
+  templateUrl: './clasificaciones.component.html',
+  styleUrl: './clasificaciones.component.scss'
+})
+export class ClasificacionesComponent {
+
+}
